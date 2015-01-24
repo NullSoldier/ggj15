bfbdc2fa3f24198c8d0c6e171bde6f7d679ed02a
Jason Spafford <nullprogrammer@gmail.com>
2015-01-23 20:21:32 -0800

    Added scaffolding

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,30 @@
-console.log("Hello World")
+var SCREEN_WIDTH  = 1280
+var SCREEN_HEIGHT = 720
+
+var game : Phaser.Game
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
+
+function render() {
+
+}
+
+window.onload = () => {
+  game = new Phaser.Game(SCREEN_WIDTH, SCREEN_HEIGHT, Phaser.AUTO, '', {
+    preload: preload,
+    create : create,
+    update : update,
+    render : render
+  })
+
+}
