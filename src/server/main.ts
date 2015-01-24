066be804d0766cc790390a0887ded24d259ebbc4
Matthew Glazar <strager@fb.com>
2015-01-23 21:35:19 -0800

    Fix server path

--- src/server/main.ts
+++ src/server/main.ts
@@ -6,7 +6,7 @@ var wslib = require('ws')
 var WebSocketServer = wslib.Server
 
 var app = express()
-app.use(express.static(__dirname + '/../..'))
+app.use(express.static(__dirname))
 app.listen(PORT, () => {
   var webSocketServer = new WebSocketServer({
     server: app,
