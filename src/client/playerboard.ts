925842986c4eee1fb86e9de527667ff8d1aeaa7c
Matthew Glazar <strager@fb.com>
2015-01-25 15:29:40 -0800

    rewrite player board

--- src/client/playerboard.ts
+++ src/client/playerboard.ts
@@ -2,13 +2,10 @@ class PlayerBoardEntry {
   nameLabel : Phaser.Text
   teamSizeLabel : Phaser.Text
 
-  constructor(public teamID : number, board : PlayerBoard) {
-    var color = witch.getTeamColor(teamID)
+  constructor(public player : Player, board : PlayerBoard) {
     var textStyle : any = {
-      fill: makeHexColor(color[0], color[1], color[2]),
       font: 'bold 20px "Helvetica"',
     }
-
     this.nameLabel = game.add.text(0, 0, '', textStyle)
     this.teamSizeLabel = game.add.text(0, 0, '', textStyle)
     this.nameLabel.x = 40
@@ -17,19 +14,19 @@ class PlayerBoardEntry {
     board.sprite.addChild(this.teamSizeLabel)
   }
 
+  count() : number {
+    return witch.getTeamPlayers(this.player.teamID).length
+  }
+
   render() : void {
-    var leader = witch.getPlayerByIDOrNull(this.teamID)
-    assertNotNull(leader)
-    this.nameLabel.text = leader.name
-
-    var teamPlayers = witch.getTeamPlayers(this.teamID)
-    this.teamSizeLabel.text = '' + teamPlayers.length
-
-    if (teamPlayers.length < 2) {
+    this.nameLabel.text = '' + this.player.name
+    if (this.player.teamID === null) {
+      this.teamSizeLabel.text = ''
       this.nameLabel.fill = '#FFFFFF'
       this.teamSizeLabel.fill = '#FFFFFF'
     } else {
-      var color = witch.getTeamColor(this.teamID)
+      this.teamSizeLabel.text = '' + this.count()
+      var color = witch.getTeamColor(this.player.teamID)
       var hex = makeHexColor(color[0], color[1], color[2])
       this.nameLabel.fill = hex
       this.teamSizeLabel.fill = hex
@@ -51,35 +48,50 @@ class PlayerBoard {
   }
 
   update() : void {
-    // Prune old teams.
+    // Prune old entries.
     for (var i = 0; i < this.entries.length; ++i) {
-      if (witch.getTeamPlayers(this.entries[i].teamID).length < 1) {
+      var e = this.entries[i]
+      if (witch.getPlayerByIDOrNull(e.player.id) === null) {
         this.entries[i].remove()
         this.entries.splice(i, 1)
         i -= 1  // =D
       }
     }
 
-    // Add new teams.
-    var existingEntryTeamIDs : Array<number> = this.entries.map((entry) => entry.teamID)
+    // Add new entries.
+    var existingEntryIDs : Array<number> = this.entries.map((entry) => entry.player.id)
     for (var i = 0; i < witch.players.length; ++i) {
       var p = witch.players[i]
-      if (p.teamID !== null && existingEntryTeamIDs.indexOf(p.teamID) === -1) {
-        if (witch.getTeamPlayers(p.teamID).length >= 1) {
-          this.entries.push(new PlayerBoardEntry(p.teamID, this))
-          existingEntryTeamIDs.push(p.teamID)
-        }
+      if (existingEntryIDs.indexOf(p.id) === -1) {
+        this.entries.push(new PlayerBoardEntry(p, this))
+        existingEntryIDs.push(p.id)  // Unnecessary (I think)
       }
     }
+
+    this.entries.sort((a : PlayerBoardEntry, b : PlayerBoardEntry) => {
+      if (a.count() > b.count()) return 1
+      if (a.count() < b.count()) return -1
+
+      if (a.player.name > b.player.name) return 1
+      if (a.player.name < b.player.name) return -1
+      return 0
+    })
   }
 
   render() : void {
     var y = 10
     this.entries.forEach((entry) => {
-      entry.render()
-      entry.nameLabel.y = y
-      entry.teamSizeLabel.y = y
-      y += 24
+      if (entry.player.teamID !== null && !entry.player.isLeader()) {
+        entry.nameLabel.visible = false
+        entry.teamSizeLabel.visible = false
+      } else {
+        entry.render()
+        entry.nameLabel.visible = true
+        entry.teamSizeLabel.visible = true
+        entry.nameLabel.y = y
+        entry.teamSizeLabel.y = y
+        y += 24
+      }
     })
   }
 }
